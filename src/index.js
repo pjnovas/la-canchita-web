071aabf7f92cc0ecb5aa3d17ec5857dccca12d94
Pablo Novas <pjnovas@gmail.com>
2015-08-04 19:43:14 -0300

    removed old files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import plugins from "./plugins";
 
-import Theme from "./Theme";
-window.Theme = Theme;
-
 import Locales from "./locales";
 var locales = new Locales("es-AR");
 window.__ = locales.strings;
