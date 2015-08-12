5f926e0ea03812fc27ac69c5c965fd1a21ca79c1
Pablo Novas <pjnovas@gmail.com>
2015-08-11 21:19:55 -0300

    added info of meeting

--- src/components/meeting/View.jsx
+++ src/components/meeting/View.jsx
@@ -3,8 +3,10 @@ import MeetingStore from "../../stores/Meeting";
 import MeetingActions from "../../actions/Meeting";
 
 import Header from "../Header.jsx";
-
 import ReactListener from "../ReactListener";
+
+import { Grid, Row, Col, ListGroup } from "react-bootstrap";
+import { ActionButton } from "../controls";
 
 export default class MeetingView extends ReactListener {
 
@@ -30,11 +32,50 @@ export default class MeetingView extends ReactListener {
   }
 
   render() {
+    let myRole = this.state.me && this.state.me.role || "member";
+    let canEdit = this.editors.indexOf(myRole) > -1;
+    let canDestroy = this.destroyers.indexOf(myRole) > -1;
+
+    let meeting = this.state.meeting;
+
+    if (!meeting){
+      return (<span>{__.loading}</span>);
+    }
+
+    let when = moment(meeting.when);
+    let time = when.from();
+    let when_str = when.format(__.full_datetime_format);
 
     return (
       <div>
-        <Header backto="group" backparams={ { groupId: this.state.gid }} />
-        Un PARTIDO!
+        <Header backto="group" backparams={ { groupId: this.state.gid } } />
+        <Grid>
+
+          <Row>
+            <Col xs={10}>
+              <h1>{meeting.title}</h1>
+            </Col>
+            <Col xs={2}>
+              <p>{when_str}</p>
+              <p>{time}</p>
+            </Col>
+          </Row>
+
+          <Row className="collapser">
+            <Col xs={6}>
+              <p>{meeting.info}</p>
+            </Col>
+            <Col xs={6}>
+              map
+            </Col>
+          </Row>
+
+          { canEdit ?
+            <ActionButton bsStyle="primary" icon="pencil"
+              to="meetingedit" params={{meetingId: this.state.id}}/>
+          : null }
+
+        </Grid>
       </div>
     );
   }
